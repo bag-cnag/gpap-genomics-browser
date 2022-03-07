d56f45e3dfcd9f030b800b7b6b89ff997e31c39c
alberto1189 <alberto.corvo89@gmail.com>
2022-03-07 18:06:11 +0100

    added license

--- client/app/src/Main.js
+++ client/app/src/Main.js
@@ -1,11 +1,28 @@
 
 
-import React, {Component} from "react";
+/* 
+<GPAP Genomics Browser, a standalone react application that embeds an IGV.js browser for visualization of genomic alignments>
+   Copyright (C) <2022>  <CRG-CNAG>
 
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Affero General Public License as published
+   by the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+For more information you can contact the authors at platform@rd-connect.eu  */
+
+import React, {Component} from "react";
 import PageHeader from "./PageHeader/index";
 import { Route, Redirect } from 'react-router-dom'
 import IGV_Page from "./scenes/igv-page/IGV_Page";
-
 import Login from "./session/Login";
 import Home from "./scenes/Home";
 import auth from "./session/Auth";
--- client/app/src/footer/Footer.js
+++ client/app/src/footer/Footer.js
@@ -1,3 +1,26 @@
+
+
+
+/* 
+<GPAP Genomics Browser, a standalone react application that embeds an IGV.js browser for visualization of genomic alignments>
+   Copyright (C) <2022>  <CRG-CNAG>
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Affero General Public License as published
+   by the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+For more information you can contact the authors at platform@rd-connect.eu  */
+
+
 import React from "react";
 
 
